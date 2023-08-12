ba0b7f4b3100da526bfef373b33da0de3b47d391
okadakk <k.suke.jp1990@gmail.com>
2023-08-12 18:53:11 +0900

    fix 500

---
+++ apps/web/app/about/page.tsx
@@ -0,0 +1,26 @@
+export const dynamic = "force-dynamic";
+
+import { use } from "react";
+import { Hello } from "shared";
+import { Button, Header } from "ui";
+
+async function getData(): Promise<Hello> {
+  const res = await fetch("http://127.0.0.1:3000");
+  if (res.status != 200) {
+    console.error("error");
+    return { message: "error" };
+  }
+  return res.json();
+}
+
+export default function Page() {
+  const data: Hello = use(getData());
+
+  return (
+    <>
+      <Header text="Web" />
+      {data?.message}
+      <Button />
+    </>
+  );
+}
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -4,22 +4,10 @@ import { use } from "react";
 import { Hello } from "shared";
 import { Button, Header } from "ui";
 
-async function getData(): Promise<Hello> {
-  const res = await fetch("http://127.0.0.1:3001/1");
-  if (res.status != 200) {
-    console.error("error");
-    return { message: "error" };
-  }
-  return res.json();
-}
-
 export default function Page() {
-  const data: Hello = use(getData());
-
   return (
     <>
       <Header text="Web" />
-      {data?.message}
       <Button />
     </>
   );
